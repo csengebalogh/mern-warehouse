2a363ffe0e1960b4a3c1e09433fccf343abfd1bc
csengebalogh <csenge.balogh1214@gmail.com>
2022-05-15 18:05:54 +0200

    useState and spreading product data

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,25 +1,68 @@
-import React from "react"
+import React, {useState} from "react"
 
+import FileBase from 'react-file-base64'
 function AddForm() {
+    const [productData, setProductData] = useState({
+        name: '',
+        supplier: '',
+        price: 0
+    })
+
+    const handleSubmit = () => {
+
+    }
+
+    const clear = () => {
+        
+    }
+
     return (
-        <form className="mb-2">
+        <form className="mb-2" noValidate >
             <h3>Add new product</h3>
             <div className="row">
                 <div className="mb-3 col">
                     <label htmlFor="prodName" className="form-label">Product Name</label>
-                    <input type="text" className="form-control" id="prodName" aria-describedby="emailHelp" />
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="prodName"
+                        value={productData.name}
+                        onChange={(e) => setProductData({...productData, name: e.target.value})}/>
                 </div>
                 <div className="mb-3 col">
                     <label htmlFor="prodSupplier" className="form-label">Product Supplier</label>
-                    <input type="text" className="form-control" id="prodSupplier" />
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="prodSupplier"
+                        value={productData.supplier}
+                        onChange={(e) => setProductData({...productData, supplier: e.target.value})}/>
                 </div>
                 <div className="mb-3 col">
-                <label htmlFor="prodPrice" className="form-label">Product Price</label>
-                <input type="number" className="form-control" id="prodPrice" />
+                    <label htmlFor="prodPrice" className="form-label">Product Price</label>
+                    <input 
+                        type="number" 
+                        className="form-control" 
+                        id="prodPrice"
+                        value={productData.price}
+                        onChange={(e) => setProductData({...productData, price: e.target.value})}/>
             </div>
+            {/* <FileBase 
+                type="file"
+                multiple={false}
+                onDone={({base64}) => setProductData({ ...productData, selectedFile: base64})}
+            /> */}
             </div>
 
-            <button type="submit" className="btn btn-primary w-100">Submit</button>
+            <button 
+                type="submit" 
+                className="btn btn-primary w-70">Submit
+            </button>
+            <button 
+                type="submit" 
+                className="btn btn-secondary w-30"
+                onClick={clear}>Clear
+            </button>
         </form>
     )
 }
