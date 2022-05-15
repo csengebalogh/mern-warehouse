fd1d1fe8532fa77cc99678536c34fdb679da6673
csengebalogh <csenge.balogh1214@gmail.com>
2022-05-15 21:49:13 +0200

    Form appears in modal page

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -3,6 +3,11 @@ import React, {useState} from "react"
 import { useDispatch } from 'react-redux'
 import { createProduct } from "../actions/products"
 
+import { 
+  Button,
+  Modal
+} from 'react-bootstrap'
+
 // import FileBase from 'react-file-base64'
 function AddForm() {
     const [productData, setProductData] = useState({
@@ -10,6 +15,11 @@ function AddForm() {
         supplier: '',
         price: 0
     })
+
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
 
     const dispatch = useDispatch()
 
@@ -20,59 +30,68 @@ function AddForm() {
     }
 
     const clear = () => {
-        
     }
 
     return (
-        <form className="mb-2" noValidate >
-            <h3>Add new product</h3>
-            <div className="row">
-                <div className="mb-3 col">
-                    <label htmlFor="prodName" className="form-label">Product Name</label>
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        id="prodName"
-                        value={productData.name}
-                        onChange={(e) => setProductData({...productData, name: e.target.value})}/>
-                </div>
-                <div className="mb-3 col">
-                    <label htmlFor="prodSupplier" className="form-label">Product Supplier</label>
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        id="prodSupplier"
-                        value={productData.supplier}
-                        onChange={(e) => setProductData({...productData, supplier: e.target.value})}/>
-                </div>
-                <div className="mb-3 col">
-                    <label htmlFor="prodPrice" className="form-label">Product Price</label>
-                    <input 
-                        type="number" 
-                        className="form-control" 
-                        id="prodPrice"
-                        value={productData.price}
-                        onChange={(e) => setProductData({...productData, price: e.target.value})}/>
-            </div>
-            {/* <FileBase 
-                type="file"
-                multiple={false}
-                onDone={({base64}) => setProductData({ ...productData, selectedFile: base64})}
-            /> */}
-            </div>
+<>
+      <Button variant="primary" onClick={handleShow}>
+        Add new product
+      </Button>
 
-            <button 
-                type="submit" 
-                className="btn btn-primary w-70"
-                onClick={handleSubmit}>Submit
-            </button>
-            <button 
-                type="submit" 
-                className="btn btn-secondary w-30"
-                onClick={clear}>Clear
-            </button>
-        </form>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add new product</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <form className="mb-2" noValidate >
+<div className="row">
+    <div className="mb-3 col">
+        <label htmlFor="prodName" className="form-label">Product Name</label>
+        <input 
+            type="text" 
+            className="form-control" 
+            id="prodName"
+            value={productData.name}
+            onChange={(e) => setProductData({...productData, name: e.target.value})}/>
+    </div>
+    <div className="mb-3 col">
+        <label htmlFor="prodSupplier" className="form-label">Product Supplier</label>
+        <input 
+            type="text" 
+            className="form-control" 
+            id="prodSupplier"
+            value={productData.supplier}
+            onChange={(e) => setProductData({...productData, supplier: e.target.value})}/>
+    </div>
+    <div className="mb-3 col">
+        <label htmlFor="prodPrice" className="form-label">Product Price</label>
+        <input 
+            type="number" 
+            className="form-control" 
+            id="prodPrice"
+            value={productData.price}
+            onChange={(e) => setProductData({...productData, price: e.target.value})}/>
+</div>
+
+</div>
+
+ </form> 
+        </Modal.Body>
+        <Modal.Footer>
+          <Button type="submit"     className="btn btn-secondary w-70"
+variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary"  className="btn btn-primary w-70" onClick={handleSubmit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
     )
 }
 
 export default AddForm
+
+
+
