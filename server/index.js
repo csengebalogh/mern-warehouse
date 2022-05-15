e2cab1c626593b3308283eac19d2a313a4fd0b98
csengebalogh <csenge.balogh1214@gmail.com>
2022-05-15 14:38:50 +0200

    Server connected to DB

--- server/index.js
+++ server/index.js
@@ -3,3 +3,23 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+const app = express()
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+
+app.use(cors())
+
+//connect server to db
+// mongoDB cluster name: ClusterOne
+
+const CONNECTION_URL = 'mongodb+srv://csengebalogh:csengebalogh@clusterone.pok0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => {app.listen(PORT, () => console.log(`Server running on port ${PORT}`))})
+    .catch((error) => console.log(error.message))
+
+
+// mongoose.set('useFindAndMxodify', false)
